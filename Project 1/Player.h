98adeda382b26e59395b2b056d674d08062c8c6d
Sragvi Vadali <58544815+sragvivadali@users.noreply.github.com>
2023-01-16 18:43:33 -0800

    project1

---
+++ Project 1/Player.h
@@ -0,0 +1,36 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <iostream>
+#include <string>
+#include <random>
+#include <utility>
+#include <cstdlib>
+#include <cctype>
+
+class Arena;
+
+class Player
+{
+  public:
+      // Constructor
+    Player(Arena* ap, int r, int c);
+
+      // Accessors
+    int  row() const;
+    int  col() const;
+    bool isDead() const;
+
+      // Mutators
+    std::string dropPoisonedCarrot();
+    std::string move(int dir);
+    void   setDead();
+
+  private:
+    Arena* m_arena;
+    int    m_row;
+    int    m_col;
+    bool   m_dead;
+};
+
+#endif
---
+++ Project 1/Rabbit.h
@@ -0,0 +1,30 @@
+#ifndef RABBIT_H
+#define RABBIT_H
+
+#include <iostream>
+#include <string>
+
+class Arena;
+
+class Rabbit {
+public:
+  // Constructor
+  Rabbit(Arena *ap, int r, int c);
+
+  // Accessors
+  int row() const;
+  int col() const;
+  bool isDead() const;
+
+  // Mutators
+  void move();
+
+private:
+  Arena *m_arena;
+  int m_row;
+  int m_col;
+  int m_health;
+  int m_idleTurnsRemaining;
+};
+
+#endif
